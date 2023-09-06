a2e2690fd9226d7d37b0bb7ad8c2b83fbde78f43
facup14 <facundo@inft.com.ar>
2023-09-06 14:05:49 -0300

    v7

--- js/script.js
+++ js/script.js
@@ -1,32 +1,36 @@
 function cotizar() {
 
-    let valore = parseInt(document.getElementById("valor").value);
+    const valorConvertir = parseInt(document.getElementById("valor").value);
     let resultado = 0;
     let dolar = 730;
     let euro = 770;
     let real = 71.88
     let chileno = 2.44
 
-    if (valore == 0) {
+    if (valorConvertir == 0) {
         alert("No puedes convertir ese valor.")
         return;
     }
 
     if (document.getElementById("uno").checked ){
-        resultado = valore / dolar;
+        resultado = valorConvertir / dolar;
         alert("El cambio de pesos argentinos a dolares es: u$d " + resultado.toFixed(2))
+        guardarConversion(valorConvertir, "ARS", "U$D", resultado);
     }
     else if (document.getElementById("dos").checked) {
-        resultado = valore / euro;
+        resultado = valorConvertir / euro;
         alert("El cambio de pesos argentinos a euros es: E$ " + resultado.toFixed(2))
+        guardarConversion(valorConvertir, "ARS", "€", resultado);
     }
     else if (document.getElementById("tres").checked) {
-        resultado = valore / real;
+        resultado = valorConvertir / real;
         alert("El cambio de pesos argentinos a reales es R$ " + resultado.toFixed(2))
+        guardarConversion(valorConvertir, "ARS", "R$", resultado);
     }
     else if (document.getElementById("cuatro").checked) {
-        resultado = valore * chileno; //multiplico porque la moneda chilena es inferior a la argentina a diferencia de las otras.
+        resultado = valorConvertir * chileno; //multiplico porque la moneda chilena es inferior a la argentina a diferencia de las otras.
         alert("El cambio de pesos argentinos a chilenos es $ " + resultado.toFixed(2))
+        guardarConversion(valorConvertir, "ARS", "CHL", resultado);
     }
     else {
         alert("Tienes que completar los campos para poder convertir")
@@ -90,3 +94,53 @@ const fechaFormateada = `${dia}/${mes}/${año}`;
 
 const elementoFecha = document.getElementById("fecha");
 elementoFecha.textContent = `Valores tomados a la fecha: ${fechaFormateada}`;
+
+
+
+
+//GUARDO LA CONVERSION EN EL LOCALSTORAGE CADA VEZ QUE REALICE UNA CONVERSION
+function guardarConversion(monto, monedaOrigen, monedaDestino, resultado) {
+    const conversion = {
+        fecha: new Date().toLocaleString(),
+        montoConvertido: monto,
+        monedaOrigen: monedaOrigen,
+        monedaDestino: monedaDestino,
+        resultado: resultado.toFixed(2),
+    };
+
+    const historial = JSON.parse(localStorage.getItem('historial')) || [];
+
+    historial.push(conversion);
+
+    localStorage.setItem('historial', JSON.stringify(historial));
+
+    actualizarTablaHistorial(historial);
+}
+
+function actualizarTablaHistorial(historial) {
+    const historialTabla = document.getElementById('historialTabla');
+    historialTabla.innerHTML = '';
+
+    historial.forEach(conversion => {
+        const fila = document.createElement('tr');
+        fila.innerHTML = `
+            <td>${conversion.fecha}</td>
+            <td>${conversion.montoConvertido} ${conversion.monedaOrigen}</td>
+            <td>${conversion.resultado} ${conversion.monedaDestino}</td>
+        `;
+        historialTabla.appendChild(fila);
+    });
+}
+
+
+//BORRAR HISTORIAL
+function borrarHistorial() {
+    localStorage.removeItem('historial');
+    actualizarTablaHistorial([]);
+}
+
+const botonBorrarHistorial = document.getElementById('borrarHistorial');
+
+botonBorrarHistorial.addEventListener('click', borrarHistorial);
+
+
